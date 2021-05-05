d086f90eba84afb2381f5aac78def00fe26b5393
Javier Alexis Salazar Ballesteros <javiersalazar1945@outlook.com>
2021-05-04 20:26:57 -0600

    Se añadio informacion para el instalador

--- UI_Plazos/Properties/AssemblyInfo.cs
+++ UI_Plazos/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("UI_Plazos")]
+[assembly: AssemblyTitle("Plazos de Visita Domiciliaria")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("UI_Plazos")]
+[assembly: AssemblyCompany("Javier Salazar Ballesteros")]
+[assembly: AssemblyProduct("Plazos de Visita Domiciliaria")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
